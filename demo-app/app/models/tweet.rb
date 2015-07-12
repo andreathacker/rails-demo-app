b875072535fd6a8756d82c7f9abeb4777ed68d63
David Thacker <davidargylethacker@gmail.com>
2015-07-11 21:46:33 -0500

    Tweets model and controller logic moved to generated classes

--- demo-app/app/models/tweet.rb
+++ demo-app/app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ActiveRecord::Base
+  validates_presence_of :status
+  validates :status,
+            presence: true
+
+  belongs_to :zombie
 end
--- demo-app/app/models/tweet_orig.rb
+++
@@ -1,9 +0,0 @@
-class Tweet < ActiveRecord::Base
-
-  validates_presence_of :status
-  validates :status,
-            presence: true
-
-  belongs_to :zombie
-
-end
