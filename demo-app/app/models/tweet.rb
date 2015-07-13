9466b96f7dacaa40646720e34750423b4d295f6c
David Thacker <davidargylethacker@gmail.com>
2015-07-13 09:32:44 -0500

    Added validation for location attribute on the Tweet model

--- demo-app/app/models/tweet.rb
+++ demo-app/app/models/tweet.rb
@@ -1,5 +1,10 @@
 class Tweet < ActiveRecord::Base
+
+  # Define the attributes that must always be available
   validates_presence_of :status
+  validates_presence_of :location
+
+  # or like this
   validates :status,
             presence: true
 
