f90e849acca16f187b65975b41bc07ba7c5faaa9
David Thacker <davidargylethacker@gmail.com>
2015-07-30 22:03:08 -0500

    Added annotations to the models to show model schema

--- demo-app/app/models/tweet.rb
+++ demo-app/app/models/tweet.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :integer          not null, primary key
+#  status     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  location   :text
+#
+
 class Tweet < ActiveRecord::Base
   validates_presence_of :status
   validates :status,
--- demo-app/test/models/tweet_test.rb
+++ demo-app/test/models/tweet_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :integer          not null, primary key
+#  status     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  location   :text
+#
+
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
